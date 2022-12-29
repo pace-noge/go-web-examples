333c76047d3cf1c51d0e83e901510f98eebb9fae
Nasaruddin <nasaruddin@mekari.com>
2022-12-29 20:48:25 +0700

    Templates

--- main.go
+++ main.go
@@ -1,120 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"time"
-
-	"database/sql"
-
-	_ "github.com/go-sql-driver/mysql"
-
-	"github.com/gorilla/mux"
+	"text/template"
 )
 
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
 func main() {
-	db, err := sql.Open("mysql", "root:root123@(127.0.0.1:3306)/go_web_examples?parseTime=true")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := db.Ping(); err != nil {
-		log.Fatal(err)
-	}
-
-	{
-		query := `
-			CREATE TABLE IF NOT EXISTS users (
-				id INT AUTO_INCREMENT,
-				username TEXT NOT NULL,
-				password TEXT NOT NULL,
-				created_at DATETIME,
-				PRIMARY KEY (id)
-			);
-		`
-		if _, err := db.Exec(query); err != nil {
-			log.Fatal(err)
+	tmpl := template.Must(template.ParseFiles("templates/layout.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My Todo List",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: false},
+				{Title: "Task 3", Done: true},
+			},
 		}
-	}
-
-	{
-		username := "johndoe"
-		password := "secret"
-		createdAt := time.Now()
-
-		result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		id, err := result.LastInsertId()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id)
-	}
-
-	{
-		var (
-			id        int
-			username  string
-			password  string
-			createdAt time.Time
-		)
-		query := "SELECT id, username, password, created_at FROM users WHERE id = ?"
-		if err := db.QueryRow(query, 1).Scan(&id, &username, &password, &createdAt); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id, username, password, createdAt)
-	}
-
-	{
-		type user struct {
-			id        int
-			username  string
-			password  string
-			createdAt time.Time
-		}
-
-		rows, err := db.Query(`SELECT id, username, password, created_at FROM users`)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer rows.Close()
-
-		var users []user
-		for rows.Next() {
-			var u user
-			err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
-			if err != nil {
-				log.Fatal(err)
-			}
-			users = append(users, u)
-		}
-
-		if err := rows.Err(); err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%#v", users)
-	}
-	{
-		_, err := db.Exec(`DELETE FROM users WHERE id = ?`, 1)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	r := mux.NewRouter()
-
-	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		title := vars["title"]
-		page := vars["page"]
-		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+		tmpl.Execute(w, data)
 	})
 
-	http.ListenAndServe(":80", r)
+	http.ListenAndServe(":80", nil)
 }
