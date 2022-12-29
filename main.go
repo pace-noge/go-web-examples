9826f51bfffee87301685f56f14580a1784227ca
Nasaruddin <nasaruddin@mekari.com>
2022-12-29 20:10:30 +0700

    HTTP Server

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 )
 
 func main() {
+	fs := http.FileServer(http.Dir("static/"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello you've requested: %s\n", r.URL.Path)
+		fmt.Fprint(w, "Welcome to my website")
 	})
 
 	http.ListenAndServe(":80", nil)
